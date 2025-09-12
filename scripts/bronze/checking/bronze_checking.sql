da76a401c367151bd971cdfaf0af62a34b3f316d
Luis <124122787+MLDLReyes@users.noreply.github.com>
2025-09-12 08:31:14 +0800

    Create bronze_checking.sql

---
+++ scripts/bronze/checking/bronze_checking.sql
@@ -0,0 +1,28 @@
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+
+--crm_cust_info
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+
+--Check for unwanted spaces
+-- Expectation: No Result
+
+--crm_cust_info
+SELECT 
+cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+
+--Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM bronze.crm_cust_info
